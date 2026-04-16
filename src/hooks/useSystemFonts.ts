8bd8ff7542ade499997680c096367af753c1bd02
megoRU <savin.yura2014@gmail.com>
2026-04-16 11:37:07 +0300

    Fix

--- src/hooks/useSystemFonts.ts
+++ src/hooks/useSystemFonts.ts
@@ -2,19 +2,10 @@ const FONTS = [
     'Droid Sans Mono',
     'DejaVu Sans Mono',
     'PT Mono',
-    'Andale Mono',
     'Cascadia Code',
-    'Fira Code',
     'JetBrains Mono',
-    'Meslo',
     'Source Code Pro',
-    'Source Code Pro Medium',
-    'Operator Mono Book',
-    'Operator Mono Medium',
     'Fira Mono',
-    'Fira Mono Medium',
-    'Inconsolata-g',
-    'Anonymous Pro',
     'Ubuntu Mono'
 ].sort();
 
