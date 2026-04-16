ef9429df753c8a7101ed0a7f951221648d6ffb97
megoRU <savin.yura2014@gmail.com>
2026-04-16 12:33:42 +0300

    Bump версии

--- src/types.ts
+++ src/types.ts
@@ -98,7 +98,7 @@ export interface Transfer {
 
 export type NotificationType = 'success' | 'error' | 'info';
 
-export const VERSION = '1.3.8';
+export const VERSION = '1.4.0';
 
 export interface SftpDownloadResult {
     remotePath: string;
